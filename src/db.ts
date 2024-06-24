9b33aee1b88c8f199e48734e1274dfc1e82d19d8
Dhruv Bavadiya <136903495+DhruvBavadiya@users.noreply.github.com>
2024-06-24 18:33:30 +0530

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectUserDB = async () => {
   try {
     // Connect Database
-    const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/");
+    const conn = await mongoose.connect("mongodb+srv://bavadiyadhruv:9snainzlzuhrudxy@sttldatabase.ul4vykr.mongodb.net/" || "mongodb://localhost:27017/");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
